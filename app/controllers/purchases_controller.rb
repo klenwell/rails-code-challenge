3bc1f3e0a51176bbddd30c4c94eb4c873bda0004
klenwell <klenwell@gmail.com>
2015-07-19 14:25:40 -0700

    Successfully processes valid file.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,10 +7,9 @@ class PurchasesController < ApplicationController
 
   # POST /purchases/upload
   def upload
-    # Processes uploaded file.
-    # Validate upload present
     return redirect_to purchases_path, alert: 'Please submit a file.' if params[:upload].blank?
 
+    # Processes uploaded file.
     @upload = params[:upload]
 
     # File records
@@ -18,6 +17,27 @@ class PurchasesController < ApplicationController
       valid: [],
       invalid: []
     }
+
+    SmarterCSV.process(@upload.path, {:col_sep => "\t"}).each_with_index do |row, line_num|
+      # Find or create associated model records.
+      merchant = Merchant.where(name: row[:merchant_name],
+                                address: row[:merchant_address]).first_or_create
+
+      product = Product.where(description: row[:item_description],
+                              price: row[:item_price],
+                              merchant_id: merchant.id).first_or_create
+
+      purchaser = Purchaser.where(name: row[:purchaser_name]).first_or_create
+
+      # Now we can create the new purchase record
+      Purchase.create!(count: row[:purchase_count],
+                              merchant_id: merchant.id,
+                              product_id: product.id,
+                              purchaser_id: purchaser.id)
+
+      row[:total_amount] = row[:purchase_count].to_i * row[:item_price].to_f
+      @rows[:valid] << row
+    end
   end
 
 end
