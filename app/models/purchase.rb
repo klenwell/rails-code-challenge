05389b76a9cdaa9b738c7bbc3a7e597fb3d35203
klenwell <klenwell@gmail.com>
2015-07-19 16:15:47 -0700

    Moves code to create purchase from row data from controller to model.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -12,6 +12,25 @@ class Purchase < ActiveRecord::Base
 
   before_save :compute_total_amount
 
+  def self.create_from_uploaded_row!(row)
+    # Find or create associated model records. Any invalid fields will raise an error
+    # that will be caught by transaction block.
+    merchant = Merchant.where(name: row[:merchant_name],
+                             address: row[:merchant_address]).first_or_create
+
+    product = Product.where(description: row[:item_description],
+                            price: row[:item_price],
+                            merchant_id: merchant.id).first_or_create
+
+    purchaser = Purchaser.where(name: row[:purchaser_name]).first_or_create
+
+    # Now we can create the new purchase record
+    return Purchase.create!(count: row[:purchase_count],
+                            merchant_id: merchant.id,
+                            product_id: product.id,
+                            purchaser_id: purchaser.id)
+  end
+
   private
 
   def compute_total_amount
