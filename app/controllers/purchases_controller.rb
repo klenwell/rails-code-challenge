93837758c7773677e013581c1c95bab2ac837fb6
klenwell <klenwell@gmail.com>
2015-07-19 13:27:17 -0700

    Stubs out purchases views and controller tests.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,13 @@
 class PurchasesController < ApplicationController
+
+  # GET /purchases
+  def index
+    # Shows upload form.
+  end
+
+  # POST /purchases/upload
+  def upload
+    # Processes uploaded file.
+  end
+
 end
--- test/controllers/purchases_controller_test.rb
+++ test/controllers/purchases_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PurchasesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should display index page with upload form" do
+    get :index
+    assert_response :success
+  end
+
+  test "should save data when valid tab file uploaded and display total revenue" do
+  end
+
+  test "should not save data when file with invalid data uploaded" do
+  end
 end
