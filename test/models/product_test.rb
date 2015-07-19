41d9daa4caadb63b23d87ebc2eee68889fcbdb0e
klenwell <klenwell@gmail.com>
2015-07-19 16:27:01 -0700

    Adds test for new Product model create method.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -34,4 +34,36 @@ class ProductTest < ActiveSupport::TestCase
     assert other_bobs_burger_special.valid?
   end
 
+  test "should create purchase when row data is valid" do
+    row = {
+      purchaser_name: 'Snake Plissken',
+      item_description: '$10 off $20 of food',
+      item_price: '10.0',
+      purchase_count: '2',
+      merchant_address: '987 Fake St',
+      merchant_name: "Bob's Pizza"
+    }
+
+    assert_difference('Purchase.count', +1) do
+      Purchase.create_from_uploaded_row!(row)
+    end
+  end
+
+  test "should not create purchase when row data is invalid" do
+    invalid_item_price = 'abc'
+
+    row = {
+      purchaser_name: 'Snake Plissken',
+      item_description: '$10 off $20 of food',
+      item_price: invalid_item_price,
+      purchase_count: '2',
+      merchant_address: '987 Fake St',
+      merchant_name: "Bob's Pizza"
+    }
+
+    assert_raises(ActiveRecord::StatementInvalid) {
+      Purchase.create_from_uploaded_row!(row)
+    }
+  end
+
 end
