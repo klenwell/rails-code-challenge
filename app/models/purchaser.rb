96726f6a3e90aec3542ab4ed77ed7f182e61ce67
klenwell <klenwell@gmail.com>
2015-07-19 11:25:11 -0700

    Update Purchaser validators and fixtures. Adds tests.

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -1,2 +1,5 @@
 class Purchaser < ActiveRecord::Base
+
+  validates :name, presence: true, uniqueness: true
+
 end
--- test/models/purchaser_test.rb
+++ test/models/purchaser_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PurchaserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should validate records correctly" do
+    amy = purchasers(:amy)
+    assert amy.valid?
+
+    amy.name = ''
+    assert_not amy.valid?
+    assert_equal amy.errors.to_hash[:name].first, "can't be blank"
+
+    amy.name = purchasers(:snake).name
+    assert_not amy.valid?
+    assert_equal amy.errors.to_hash[:name].first, 'has already been taken'
+  end
+
 end
