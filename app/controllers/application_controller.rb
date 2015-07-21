2ea152020b728bf2e6aedc66df3383cd517999c7
klenwell <klenwell@gmail.com>
2015-07-20 22:10:42 -0700

    Adds authenticate method to application controller and adds new auth routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  def authenticate
+    unless signed_in?
+      redirect_to auth_confirm_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'purchases', to: 'purchases#index'
+  get 'purchases', to: 'index'
   post '/purchases/upload', to: 'purchases#upload'
+
+  # Authentication paths
+  get '/authenticate', to: 'sessions#new', as: :auth_confirm
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   root 'purchases#index'
 
