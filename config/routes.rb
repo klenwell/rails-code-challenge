77c65d6b082c41a0c35421f1f9943aa1d4f862e6
klenwell <klenwell@gmail.com>
2015-07-20 22:13:19 -0700

    Adds session new view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  # Purchases upload
   get 'purchases', to: 'index'
   post '/purchases/upload', to: 'purchases#upload'
 
-  # Authentication paths
+  # Authentication
   get '/authenticate', to: 'sessions#new', as: :auth_confirm
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
