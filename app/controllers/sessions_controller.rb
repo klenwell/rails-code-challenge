71e57f6a09188d7a7ac4006817959ce85155f4b3
klenwell <klenwell@gmail.com>
2015-07-20 22:03:00 -0700

    Adds authentication methods for session controller and session helper.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    # Displays confirmation link
+  end
+
+  def create
+    google_auth = request.env["omniauth.auth"]
+
+    if google_auth
+      auth_token = google_auth[:credentials][:token]
+      sign_in auth_token
+    else
+      flash[:alert] = 'Authentication failed!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+  def sign_in(auth_token)
+    session[:auth_token] = auth_token
+    redirect_to root_path
+  end
+
+  def signed_in?
+    true if session[:auth_token]
+  end
+
+  def sign_out
+    session[:auth_token] = nil
+  end
 end
